688cd8ccd7b96c800fdd97b1b99a86aa2388520c
Priyansh Sharma <132761882+priyanshsharmatech@users.noreply.github.com>
2025-08-06 18:38:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import store from './Redux/Store.js'
+import store from './redux/Store.js'
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
